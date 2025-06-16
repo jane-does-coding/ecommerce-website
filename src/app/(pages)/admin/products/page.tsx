3cf8a082b1a452312f4e90c03d40d654a54284dc
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2025-06-16 16:38:57 -0600

    table

--- src/app/(pages)/admin/products/page.tsx
+++ src/app/(pages)/admin/products/page.tsx
@@ -1,38 +1,63 @@
-import React from "react";
-import PageHeader from "../_components/PageHeader";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import {
 	Table,
 	TableBody,
+	TableCell,
 	TableHead,
 	TableHeader,
 	TableRow,
 } from "@/components/ui/table";
+import db from "@/db/db";
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+	ActiveToggleDropdownItem,
+	DeleteDropdownItem,
+} from "./_components/ProductActions";
+import PageHeader from "../_components/PageHeader";
 
-const AdminProductsPage = () => {
+export default function AdminProductsPage() {
 	return (
 		<>
-			<div className="flex justify-between items-center">
+			<div className="flex justify-between items-center gap-4">
 				<PageHeader>Products</PageHeader>
 				<Button asChild>
-					<Link href={"/admin/products/new"}>Add Product</Link>
+					<Link href="/admin/products/new">Add Product</Link>
 				</Button>
 			</div>
 			<ProductsTable />
 		</>
 	);
-};
+}
 
-export default AdminProductsPage;
+async function ProductsTable() {
+	const products = await db.product.findMany({
+		select: {
+			id: true,
+			name: true,
+			priceInCents: true,
+			isAvailableForPurchase: true,
+			_count: { select: { orders: true } },
+		},
+		orderBy: { name: "asc" },
+	});
 
-function ProductsTable() {
+	if (products.length === 0) return <p>No products found</p>;
+
 	return (
 		<Table>
 			<TableHeader>
 				<TableRow>
 					<TableHead className="w-0">
-						<span className="sr-only">Available for Purchase</span>
+						<span className="sr-only">Available For Purchase</span>
 					</TableHead>
 					<TableHead>Name</TableHead>
 					<TableHead>Price</TableHead>
@@ -42,7 +67,57 @@ function ProductsTable() {
 					</TableHead>
 				</TableRow>
 			</TableHeader>
-			<TableBody></TableBody>
+			<TableBody>
+				{products.map((product) => (
+					<TableRow key={product.id}>
+						<TableCell>
+							{product.isAvailableForPurchase ? (
+								<>
+									<span className="sr-only">Available</span>
+									<CheckCircle2 />
+								</>
+							) : (
+								<>
+									<span className="sr-only">Unavailable</span>
+									<XCircle className="stroke-destructive" />
+								</>
+							)}
+						</TableCell>
+						<TableCell>{product.name}</TableCell>
+						<TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+						<TableCell>{formatNumber(product._count.orders)}</TableCell>
+						<TableCell className="cursor-pointer">
+							<DropdownMenu>
+								<DropdownMenuTrigger>
+									<MoreVertical />
+									<span className="sr-only">Actions</span>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent>
+									<DropdownMenuItem asChild>
+										<a download href={`/admin/products/${product.id}/download`}>
+											Download
+										</a>
+									</DropdownMenuItem>
+									<DropdownMenuItem asChild>
+										<Link href={`/admin/products/${product.id}/edit`}>
+											Edit
+										</Link>
+									</DropdownMenuItem>
+									<ActiveToggleDropdownItem
+										id={product.id}
+										isAvailableForPurchase={product.isAvailableForPurchase}
+									/>
+									<DropdownMenuSeparator />
+									<DeleteDropdownItem
+										id={product.id}
+										disabled={product._count.orders > 0}
+									/>
+								</DropdownMenuContent>
+							</DropdownMenu>
+						</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
 		</Table>
 	);
 }
